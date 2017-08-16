c37f0e09d273077e4adc00672a77742ad0f5e618
Donny2002 <higashi2002kr@gmail.com>
2017-08-16 18:38:11 +0900

    .

--- src/main/java/bigdata3/service/StudentService.java
+++ src/main/java/bigdata3/service/StudentService.java
@@ -7,3 +7,4 @@ import bigdata3.domain.Student;
 public interface StudentService {
   List<Student> list(int pageNo, int pageSize) throws Exception;
   void add(Student student) throws Exception; 
+}
