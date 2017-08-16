1585f881aad0d54fd2a500393121442b9277d25b
dngngnt98 <cho988@naver.com>
2017-08-16 17:42:28 +0900

    .

--- src/main/java/bigdata3/dao/StudentDao.java
+++ src/main/java/bigdata3/dao/StudentDao.java
@@ -1,5 +1,14 @@
 package bigdata3.dao;
 
-public class StudentDao {
+import java.util.List;
+import java.util.Map;
 
-}
+import bigdata3.domain.Student;
+
+public interface StudentDao { 
+  List<Student> selectList(Map<String,Object> valueMap); 
+  int insert(Student student);
+  Student selectOne(int no);
+  int update(Student student); 
+  int delete(int no);
+} 
